1a4c3d740b324d5bb07a68851f96c51599410623
Crypt0nic0 <n.greneche45@gmail.com>
2023-05-15 14:27:31 +0200

    cours terminé, next step : Yoga App

--- cours/index.js
+++ cours/index.js
@@ -35,3 +35,117 @@ for(const key in obj)
 
 obj.direBonjour();
 obj.direBonjour2();
+
+
+
+// OBTENIR LES CLES D'UN OBJET
+const keys = Object.keys(obj);
+console.log(keys);
+
+// OBTENIR LES VALEURS
+const values = Object.values(obj);
+console.log(values);
+
+const nestedArray = Object.entries(obj);
+console.log(nestedArray);
+
+const obj2 = {
+    taille: "1m80",
+    poids: "75kg"
+}
+
+// FUSIONNER LES OBJETS
+const fusion = Object.assign({}, obj, obj2);
+console.log(fusion);
+
+// EMPECHER LES MODIFICATIONS
+const newObj = Object.freeze(obj);
+newObj.pseudo = "test";
+console.log(newObj);
+
+
+
+//=========================================================================//
+
+//                CONSTRUCTEURS D'OBJETS
+
+//=========================================================================//
+
+// --- Fonction constructeur ---
+
+function User(pseudo, ville){
+    this.pseudo = pseudo;
+    this.ville = ville;
+
+    this.getCity = function(){
+        console.log(this.pseudo + " habite à " + this.ville);
+    }
+}
+
+const user1 = new User('Cryptonico', 'Gidy');
+console.log(user1);
+user1.getCity();
+
+// --- Factory functions ---
+
+function User3(pseudo, ville){
+    return {
+        pseudo,
+        ville
+    }
+}
+
+const user4 = User3('CryptoNico2', 'Gidy2');
+console.log(user4);
+
+
+// --- LES CLASSES ---
+
+class Utilisateur{
+    constructor(pseudo, ville){
+        this.pseudo = pseudo;
+        this.ville = ville;
+    }
+
+    // Methodes
+
+    sayMyName = function(){
+        console.log('Bonjour je suis ' + this.pseudo);
+    }
+
+}
+
+const user5 = new Utilisateur('CryptoNicoClass', 'GidyClass');
+
+Utilisateur.prototype.sayCity = function(){
+    console.log("J'habite à " + this.ville);
+}
+
+console.log(user5);
+user5.sayMyName();
+user5.sayCity();
+
+
+// --- HERITAGE ---
+
+class Animal{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    saySomething(text){
+        console.log(this.name + " dit : " + text);
+    }
+}
+
+class Dog extends Animal{
+    run(){
+        console.log("Le chien court ! ");
+    }
+}
+
+const rintintin = new Dog("rintintin", 16);
+console.log(rintintin);
+rintintin.saySomething("yeah");
+rintintin.run();
